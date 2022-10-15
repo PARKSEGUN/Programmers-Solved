996c6e7aaeb0f2d6f69c6bf651d819dfd2ade501
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2022-10-15 19:06:05 +0900

    Update H-Indx.cpp

--- Level 2/H-Indx.cpp
+++ Level 2/H-Indx.cpp
@@ -24,3 +24,23 @@ int solution(vector<int> citations) {
         if(start>citations[citations.size()-1]){return answer;}
     }
 }
+
+
+---------------참고코드-----------------
+    #include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
+    }
+
+    return citations.size();
+}
